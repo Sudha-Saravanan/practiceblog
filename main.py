49741a4b34d0446ca290bed65a6e86b30e1255ef
Sudha Saravanan <sudha_sarv@yahoo.com>
2017-12-20 00:16:24 -0600

    almost 2/3 of the project is completed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, flash
+from flask import Flask, request, redirect, render_template, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -13,20 +13,44 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(180))
     body = db.Column(db.String(1000))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     date = db.Column(db.DateTime)
 
-    def __init__(self, title, body, date=None):
+    def __init__(self, title, body, owner, date = None):
         self.title = title
         self.body = body
+        self.owner = owner
         if date is None:
             date = datetime.utcnow()
         self.date = date
+        
+
 
     def is_valid(self):
         if self.title and self.body:
             return True
         else:
             return False
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return str(self.username)
+
+@app.before_request
+def require_login():
+    allowed_routes = ['newpost', 'login', 'blog', 'index', 'register', 'individualpost', 'allpost']
+    if request.endpoint not in allowed_routes and 'username' not in session:
+        return redirect('/login')
 
 
 
@@ -36,7 +60,7 @@ def index():
     return redirect("/blog")
    
 @app.route("/blog")
-def display_blog_entries():
+def blog():
     # TODO refactor to use routes with variables instead of GET parameters
     
     if request.args:
@@ -55,11 +79,14 @@ def display_blog_entries():
 
 @app.route("/newpost", methods=['POST', 'GET'])
 def newpost():
+    welcome = "Logged in as:" + session['user']
+    owner = User.query.filter_by(username=session['username']).first()
 
     if request.method =='POST':
         newpost_title = request.form['title']
         newpost_body = request.form['body']
-        newpost= Blog(newpost_title, newpost_body)
+        
+        newpost= Blog(newpost_title, newpost_body, owner)
         
         if newpost.is_valid():
             db.session.add(newpost)
@@ -74,6 +101,59 @@ def newpost():
             return render_template('newpost.html',title="New Post")
     else:
         return render_template('newpost.html',title="New Post")
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['pwd1']
+
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            if existing_user.password == "":
+                flash("Password is required", 'error')
+                return render_template('login.html')
+            elif existing_user.password == password:
+                session['username'] = username
+                flash("Logged in")
+                return redirect("/blog")
+            else:
+                flash("Invalid password", 'error' )
+                return render_template('login.html')
+        else:
+            flash("Invalid Username", 'error')
+            return render_template('login.html')   
+            
+    return render_template('login.html', title ="Login")#, username=username)
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['pwd1']
+        verify = request.form['pwd2']
+        #TODO - add validation
+
+        existing_user = User.query.filter_by(username=username).first()
+        if not existing_user:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+
+            #TODO - remember the user
+            session['username'] = username
+            return redirect('/newpost')
+        else:
+            #TODO-user response messaging
+            return "<h1>Duplicate User</h1>"
+
+    return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    del session['username']
+    return redirect('/')
+
         
 if __name__ == '__main__':
     app.run()
