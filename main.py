7e905ff9778f7b8da8ba2bbc359ad35e44f40c72
Sudha Saravanan <sudha_sarv@yahoo.com>
2018-02-21 21:08:29 -0600

    completed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:sudha@localhost:8889/blogz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://practiceblog:sudha@localhost:8889/practiceblog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'sudha'
